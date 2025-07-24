b8662f8f0c43f628150b965046d826f4d49cf5d1
Will Chen <willchen90@gmail.com>
2025-07-24 11:56:17 -0700

    clean up db

--- drizzle/0013_confused_nitro.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `chats` ADD `db_lsn` text;--> statement-breakpoint
-ALTER TABLE `messages` ADD `db_lsn` text;
--- drizzle/0015_daily_justin_hammer.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `apps` RENAME COLUMN "neon_branch_id" TO "neon_development_branch_id";--> statement-breakpoint
-ALTER TABLE `apps` ADD `neon_preview_branch_id` text;
